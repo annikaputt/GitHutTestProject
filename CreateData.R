46cf816be92a1adc24ac122b0e13c7cf231247fe
annikaputt <annika@instream.net>
2016-02-26 14:05:28 -0800

    Tried a reset

--- CreateData.R
+++ CreateData.R
@@ -11,10 +11,15 @@
 ############################
 
 # Make some data
-fishes <- data.frame(index = 1:10, redfish = rnorm(10,20,5), bluefish = rnorm(10,10,3))
+fishes <- data.frame(index = 1:10, redfish = rnorm(10,20,5), bluefish = rnorm(10,10,3), greenfish = rnorm(10,10,10))
 
 # Plot the data
 plot(fishes$index,fishes$bluefish,col="blue",pch=19,xlab="",ylab="",ylim=c(0,30),las=1)
 points(fishes$index,fishes$redfish,col="red",pch=19)
+points(fishes$index,fishes$greenfish,col="green",pch=19)
 mtext(side=1,text="Index",line=3)
 mtext(side=2,text="Fish Count",line=3)
+
+
+# Create some text I might want to get rid of
+# Another line
