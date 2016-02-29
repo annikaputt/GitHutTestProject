d070a03acc1c9b361865859fb271c7cf605d12fc
Douglas Braun <dbraun@instream.net>
2016-02-29 14:19:45 -0800

    first colab

--- CreateData.R
+++ CreateData.R
@@ -14,9 +14,9 @@
 fishes <- data.frame(index = 1:10, redfish = rnorm(10,20,5), bluefish = rnorm(10,10,3), greenfish = rnorm(10,10,10))
 
 # Plot the data
-plot(fishes$index,fishes$bluefish,col="blue",pch=19,xlab="",ylab="",ylim=c(0,30),las=1)
-points(fishes$index,fishes$redfish,col="red",pch=19)
-points(fishes$index,fishes$greenfish,col="green",pch=19)
+plot(fishes$index,fishes$bluefish,col="#de2d26",pch=19,xlab="",ylab="",ylim=c(0,30),las=1)
+points(fishes$index,fishes$redfish,col="#fc9272",pch=19)
+points(fishes$index,fishes$greenfish,col="#fee0d2",pch=19)
 mtext(side=1,text="Index",line=3)
 mtext(side=2,text="Fish Count",line=3)
   
